fc79fd9572ae238a2874f8444acbf937b3bc9951
unknown <fredrickritchie@gmail.com>
2024-01-26 15:51:43 +0530

    inital

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,37 +1,23 @@
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
-
-  cluster_name    = "my-cluster"
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "~> 19.0"
+  cluster_name    = "myapp-eks-cluster"
   cluster_version = "1.28"
 
-  cluster_endpoint_public_access  = true
+  cluster_endpoint_public_access = true
 
-  cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
-    }
-  }
+  vpc_id     = module.myapp-vpc.vpc_id
+  subnet_ids = module.myapp-vpc.private_subnets
 
-  vpc_id                   = module.myapp-vpc.vpc_id
-  subnet_ids               = module.myapp-vpc.private_subnets
-
-  # EKS Managed Node Group(s)
-  eks_managed_node_group_defaults = {
-    instance_types = ["t2.micro"]
+  tags = {
+    environment = "development"
+    application = "myapp"
   }
 
   eks_managed_node_groups = {
-    blue = {}
-    green = {
+    dev = {
       min_size     = 1
-      max_size     = 10
+      max_size     = 5
       desired_size = 3
 
       instance_types = ["t2.micro"]
